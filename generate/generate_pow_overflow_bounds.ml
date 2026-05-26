c7d850959ed8f0e952740025734b97ef54622ba5
LSR614 <76845284+LSR614@users.noreply.github.com>
2026-05-25 23:56:58 -0400

    Update generate_pow_overflow_bounds.ml

--- generate/generate_pow_overflow_bounds.ml
+++ generate/generate_pow_overflow_bounds.ml
@@ -1,6 +1,10 @@
 (* NB: This needs to be pure OCaml (no Base!), since we need this in order to build
    Base. *)
 
+
+(* NB: This needs to be pure OCaml (no Base!), since we need this in order to build
+   Base. *)
+
 (* This module generates lookup tables to detect integer overflow when calculating integer
    exponents.  At index [e], [table.[e]^e] will not overflow, but [(table[e] + 1)^e]
    will. *)
